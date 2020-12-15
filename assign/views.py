33c96c2f3dc1b832d42a38963449e7f156351089
Info-Hua <infohuasy@gmail.com>
2020-12-15 22:24:03 +0800

    hua

--- assign/views.py
+++ assign/views.py
@@ -484,7 +484,8 @@ def admin_seat(request):
                     temp = cursor.fetchall()
                     cursor.execute("select * from Room where id=%s",[info[0]])
                     room = cursor.fetchall()
-                    # if room[0][1] != info[1] or room[0][2] != info[2]: # 进行房间行列修改
+                    if room[0][1] != info[1] or room[0][2] != info[2]: # 进行房间行列修改
+                        print(1)
                     #     if len(temp) != 0:
                     #         turn = True
                     #     else:
